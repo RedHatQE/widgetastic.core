77699a73143b158404c52c047df5d4cbff0b151a
Milan Falešník <mfalesni@redhat.com>
2016-09-06 16:21:55 +0200

    Added test_xpath.py

---
+++ testing/test_xpath.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from widgetastic.core.xpath import quote
+
+
+@pytest.mark.parametrize(
+    ('a', 'b'), [
+        ('<>', '"<>"'),
+        ('&', '"&"'),
+        ('"\'', '"&quot;\'"'),
+    ])
+def test_xpath_quote(a, b):
+    assert quote(a) == b
