7ef3e52cb2c7ef1a219a003ff9b7f87723debd66
Oleksii Tsuman <otsuman@redhat.com>
2016-09-29 16:43:47 +0300

    Add verification for empty text field

--- src/widgetastic/browser.py
+++ src/widgetastic/browser.py
@@ -392,6 +392,8 @@ class Browser(object):
             text = self.execute_script(
                 'return arguments[0].textContent || arguments[0].innerText;',
                 self.element(*args, **kwargs))
+            if text is None:
+                text = ''
 
         return normalize_space(text)
 
