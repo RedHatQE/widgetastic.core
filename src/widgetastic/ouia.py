7888153ffb2d11defe89bc4d548bc69435245c80
Dmitry Misharov <dmisharo@redhat.com>
2020-09-01 22:48:11 +0200

    Added OUIA support

---
+++ src/widgetastic/ouia.py
@@ -0,0 +1,72 @@
+from widgetastic.utils import ParametrizedLocator
+from widgetastic.xpath import quote
+from widgetastic.widget.base import Widget
+from widgetastic.widget.base import View
+from widgetastic.widget.base import ClickableMixin
+
+
+class OUIABase:
+    """
+    Base class for ``OUIA`` support. According to the spec ``OUIA`` compatible components may have
+    the following attributes in the root level HTML element:
+        * data-ouia-component-type
+        * data-ouia-component-id
+        * data-ouia-safe
+    https://ouia.readthedocs.io/en/latest/README.html#ouia-component
+    """
+
+    ROOT = ParametrizedLocator(".//*[@data-ouia-component-type={@component_type}{@component_id}]")
+
+    def set_ouia_attrs(self, component_type, component_id=None, namespace=None):
+        component_type = f"{namespace}/{component_type}" if namespace else component_type
+        self.component_type = quote(component_type)
+        component_id = f" and @data-ouia-component-id={quote(component_id)}" if component_id else ""
+        self.component_id = component_id
+        self.locator = self.ROOT.locator
+
+    @property
+    def is_safe(self):
+        """
+        An attribute called data-ouia-safe, which is True only when the component is in a static
+        state, i.e. no animations are occurring. At all other times, this value MUST be False.
+        """
+        return "true" in self.browser.get_attribute("data-ouia-safe", self)
+
+
+class OUIAGenericView(OUIABase, View):
+    """A base class for any OUIA compatible view.
+
+    Children classes must have the same name as the value of ``data-ouia-component-type`` attriburte
+    of the root HTML element. Besides children classes should define ``OUIA_NAMESPACE`` attribute if
+    it's appicable.
+
+    Args:
+        component_id: value of data-ouia-component-id attribute.
+    """
+
+    OUIA_NAMESPACE = None
+
+    def __init__(self, parent, component_id=None, logger=None, **kwargs):
+        super().set_ouia_attrs(type(self).__name__, component_id, namespace=self.OUIA_NAMESPACE)
+        super().__init__(parent, logger=logger, **kwargs)
+
+
+class OUIAGenericWidget(OUIABase, Widget, ClickableMixin):
+    """A base class for any OUIA compatible widget.
+
+    Children classes must have the same name as the value of ``data-ouia-component-type`` attriburte
+    of the root HTML element. Besides children classes should define ``OUIA_NAMESPACE`` attribute if
+    it's appicable.
+
+    Args:
+        component_id: value of data-ouia-component-id attribute.
+    """
+
+    OUIA_NAMESPACE = None
+
+    def __init__(self, parent, component_id=None, logger=None):
+        super().set_ouia_attrs(type(self).__name__, component_id, namespace=self.OUIA_NAMESPACE)
+        super().__init__(parent, logger=logger)
+
+    def __locator__(self):
+        return self.ROOT
