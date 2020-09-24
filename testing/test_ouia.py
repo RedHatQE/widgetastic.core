5b9e9c87197017a98ef9242cb2d8c0370dd7d563
Dmitry Misharov <dmisharo@redhat.com>
2020-09-24 16:27:56 +0200

    Covered is_safe property in tests

--- testing/test_ouia.py
+++ testing/test_ouia.py
@@ -37,6 +37,8 @@ def test_basic(browser, testing_view):
     assert testing_view.is_displayed
     assert testing_view.button.is_displayed
     assert testing_view.select.is_displayed
+    assert not testing_view.button.is_safe
+    assert testing_view.select.is_safe
     testing_view.button.click()
     testing_view.select.choose("first_option")
     testing_view.select.choose("second_option")
